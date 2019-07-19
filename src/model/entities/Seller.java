4391e34d604e470e2ba8b7f11e3f90c4f25b5327
erichiroshi <erichiroshi@gmail.com>
2019-07-19 10:48:54 -0400

    Seller entity class

---
+++ src/model/entities/Seller.java
@@ -0,0 +1,53 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Seller implements Serializable {
+
+	private int id;
+	private String name;
+	private String email;
+	private Date birthDate;
+	private double baseSalary;
+
+	private Department department;
+
+	public Seller(int id, String name, String email, Date birthDate, double baseSalary, Department department) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.birthDate = birthDate;
+		this.baseSalary = baseSalary;
+		this.department = department;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
+				+ baseSalary + ", department=" + department + "]";
+	}
+
+}
